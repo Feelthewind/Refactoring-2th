17bd380013f90f2922eccf10c5df15e25983bab1
Lim changsu <skyisthelimitless@gmail.com>
2020-06-11 18:32:34 +0900

    총합 부분 이동

--- statement.js
+++ statement.js
@@ -2,6 +2,9 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
@@ -10,6 +13,22 @@ function statement(invoice, plays) {
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
+  }
+
+  function totalAmount(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.amount;
+    }
+    return result;
+  }
+
+  function totalVolumeCredits(data) {
+    let volumeCredits = 0;
+    for (let perf of data.performances) {
+      volumeCredits += perf.volumeCredits;
+    }
+    return volumeCredits;
   }
 
   function playFor(aPerformance) {
@@ -48,32 +67,16 @@ function statement(invoice, plays) {
   }
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
   let result = `청구 내역 (고개명: ${data.customer})\n`;
   for (let perf of data.performances) {
     // 청구 내역을 출력한다.
     result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
 
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
   return result;
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
-  }
-
-  function totalVolumeCredits() {
-    let volumeCredits = 0;
-    for (let perf of data.performances) {
-      volumeCredits += perf.volumeCredits;
-    }
-    return volumeCredits;
-  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
