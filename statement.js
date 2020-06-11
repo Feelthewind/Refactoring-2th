8105e062ae311f7583c6cec0fbbf1822a2b8242e
Lim changsu <skyisthelimitless@gmail.com>
2020-06-11 21:51:11 +0900

    중간 데이터 구조 테스트 추가

--- statement.js
+++ statement.js
@@ -14,14 +14,28 @@ function renderPlainText(data) {
   result += `총액: ${usd(data.totalAmount)}\n`;
   result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
   return result;
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
+function htmlStatement(invoice, plays) {
+  return rednerHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+  for (let perf of data.performances) {
+    result += ` <tr></tr>`;
   }
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 }
 
 module.exports = {
--- tests/statement.test.js
+++ tests/statement.test.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
 const { statement } = require("../statement");
+const createStatementData = require("../createStatementData");
 
 test("statement", () => {
   const invoicesJsonString = fs.readFileSync("./invoices.json", "utf8");
@@ -17,4 +18,42 @@ test("statement", () => {
 `;
 
   expect(statement(invoices[0], plays)).toBe(expected);
+});
+
+test("createStatementData", () => {
+  const invoicesJsonString = fs.readFileSync("./invoices.json", "utf8");
+  const invoices = JSON.parse(invoicesJsonString);
+  const playsJsonString = fs.readFileSync("./plays.json", "utf8");
+  const plays = JSON.parse(playsJsonString);
+
+  const expected = {
+    customer: "BigCo",
+    performances: [
+      {
+        amount: 65000,
+        audience: 55,
+        play: { name: "Hamlet", type: "tragedy" },
+        playID: "hamlet",
+        volumeCredits: 25,
+      },
+      {
+        amount: 49000,
+        audience: 35,
+        play: { name: "As you Like It", type: "comedy" },
+        playID: "as-like",
+        volumeCredits: 12,
+      },
+      {
+        amount: 50000,
+        audience: 40,
+        play: { name: "Othello", type: "tragedy" },
+        playID: "othello",
+        volumeCredits: 10,
+      },
+    ],
+    totalAmount: 164000,
+    totalVolumeCredits: 47,
+  };
+
+  expect(createStatementData(invoices[0], plays)).toStrictEqual(expected);
 });
