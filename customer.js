7aaabc20fb70acb9d3e32fc15997f9c8e4475039
Lim changsu <skyisthelimitless@gmail.com>
2020-06-14 18:04:52 +0900

    필드 옮기기

--- customer.js
+++ customer.js
@@ -1,24 +1,38 @@
 class Customer {
   constructor(name, discountRate) {
     this._name = name;
-    this._discountRate = discountRate;
     this._contract = new CustomerContract(new Date());
+    this._setDiscountRate(discountRate);
   }
 
   get discountRate() {
     return this._discountRate;
   }
+
+  _setDiscountRate(aNumber) {
+    this._contract.discountRate = aNumber;
+  }
+
   becomePreferred() {
     this._discountRate += 0.03;
   }
   applyDiscount(amount) {
-    return amount.substract(amount.multiply(this._discountRate));
+    return amount.substract(amount.multiply(this._contract.discountRate));
   }
 }
 
 class CustomerContract {
-  constructor(startDate) {
+  constructor(startDate, discountRate) {
     this._startDate = startDate;
+    this._discountRate = discountRate;
+  }
+
+  get discountRate() {
+    return this._discountRate;
+  }
+
+  set discountRate(arg) {
+    this._discountRate = arg;
   }
 }
 
